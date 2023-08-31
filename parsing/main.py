bc81ade2da382984b0326845fbe96e2030c2be8b
Khachkara <khachkara@gmail.com>
2023-08-31 23:30:13 +0300

    получение всех вакансий

--- parsing/main.py
+++ parsing/main.py
@@ -1,74 +1,122 @@
 import requests
+
+
 import json
 import time
 import csv
 
-def get_vacancy(text, page=0):
-    params = {
-        'text': text, 
-    } 
-    try:
-        req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
-        data = req.content.decode()
-        out = json.loads(data)['items']
-        fields = ["id", "name", "responsibility", "salary", "address", "employment", "employer", "professional_roles", "alternate_url"]
-        
-        vacancy = {}
 
-        if not out[page]["id"]:
+
+def get_all_vacancies(text):
+    """
+
+    собирает все вакансии и получает количество страниц page_vacansies
+
+    возвращает все вакансии 
+    """
+
+    vacansies, pages = get_vacancies(text)
+
+    for page in range(1, pages):
+
+        page_vacansies, _ = get_vacancies(text, page)
+
+        vacansies.extend(page_vacansies)
+
+    return vacansies
+
+
+
+def get_vacancies(text, page=0):
+    """
+
+    возвращает объявления на конкретной страниуе
+    """
+
+
+    params = { # параметры обращения к api
+
+
+        'text': text,  # искомая вакансия
+
+
+        'per_page': 20, # кол. вакансий на страницу
+
+
+        'page': page  # номер страницы
+
+
+    } 
+
+    # ответ api 
+    response = requests.get('https://api.hh.ru/vacancies', params)  
+
+    # декодированные данные (строка)
+    data = response.content.decode()
+
+    # данные в формате json
+    response_data = json.loads(data)
+
+    # общая количество страниц
+    pages = response_data['pages']
+
+    # вакансии на текущей странице
+    items = response_data['items']
+ 
+    # fields = ["id", "name", "responsibility", "salary", "address", "employment", "employer", "professional_roles", "alternate_url"]
+   
+    vacansies = []
+
+    for item in items:
+        vacancy = {}
+        if not item["id"]:
             vacancy["id"] = 'null'
         else:
-            vacancy["id"] = out[page]["id"]
-        
-        if not out[page]["name"]:
+            vacancy["id"] = item["id"]
+
+        if not item["name"]:
             vacancy["name"] = 'null'
         else:
-            vacancy["name"] = out[page]["name"]
-        
-        if not out[page]["snippet"]["responsibility"]:
+            vacancy["name"] = item["name"]
+
+        if not item["snippet"]["responsibility"]:
             vacancy["responsibility"] = 'null'
         else:
-            vacancy["responsibility"] = out[page]["snippet"]["responsibility"]
+            vacancy["responsibility"] = item["snippet"]["responsibility"]
 
-        if not out[page]["salary"]:
+        if not item["salary"]:
             vacancy["salary"] = 'null'
         else:
-            vacancy["salary"] = out[page]["salary"]["from"]
+            vacancy["salary"] = item["salary"]["from"]
 
-        if not out[page]["address"]:
+        if not item["address"]:
             vacancy["address"] = 'null'
         else:
-            vacancy["address"] = out[page]["address"]["name"]
+            vacancy["address"] = item["address"]["city"]
 
-        if not out[page]["employment"]:
+        if not item["employment"]:
             vacancy["employment"] = 'null'
         else:
-            vacancy["employment"] = out[page]["employment"]["name"]
-        
-        if not out[page]["employer"]:
+            vacancy["employment"] = item["employment"]["name"]
+
+        if not item["employer"]:
             vacancy["employer"] = 'null'
         else:
-            vacancy["employer"] = out[page]["employer"]["name"]
+            vacancy["employer"] = item["employer"]["name"]
 
-        if not out[page]["professional_roles"]:
+        if not item["professional_roles"]:
             vacancy["professional_roles"] = 'null'
         else:
-            vacancy["professional_roles"] = out[page]["professional_roles"][0]["name"]
+            vacancy["professional_roles"] = item["professional_roles"][0]["name"]
+
+        vacansies.append(vacancy)
+
+    return vacansies, pages
 
 
-        # vacancy = {
-        #     "id": out[page]["id"],
-        #     "name": out[page]["name"],
-        #     "responsibility": out[page]["snippet"]["responsibility"],
-        #     "salary": out[page]["salary"]["from"],
-        #     "address": out[page].get("address"),
-        #     "employment": out[page]["employment"]["name"],
-        #     "employer": out[page]["employer"]["name"],
-        #     "professional_roles": out[page]["professional_roles"][0]["name"],
-        # }
-    except Exception as e:
-        print(e)
-    return vacancy
+result = get_all_vacancies("стажер C++")
 
-print(json.dumps(get_vacancy("стажер C++"), indent=4, ensure_ascii=False))
-# get_vacancy("стажер C++")
+print(json.dumps(result, indent=4, ensure_ascii=False))
+
+print(len(result))
+
