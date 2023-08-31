1d600b1ee509eded5cacb565340fd504564dcaa9
khachkara@gmail.com <khachkara@gmail.com>
2023-08-31 13:22:18 +0300

    данные получены

--- parsing/main.py
+++ parsing/main.py
@@ -12,23 +12,63 @@ def get_vacancy(text, page=0):
         data = req.content.decode()
         out = json.loads(data)['items']
         fields = ["id", "name", "responsibility", "salary", "address", "employment", "employer", "professional_roles", "alternate_url"]
-        print(json.dumps(out[page], indent=4))
-        address = out[page].get("address")
-        # todo написать условие проверяюшее наличие данных в поле
-        vacancy = {
-            "id": out[page]["id"],
-            "name": out[page]["name"],
-            "responsibility": out[page]["snippet"]["responsibility"],
-            "salary": out[page]["salary"]["from"],
-            "address": out[page].get("address"),
-            "employment": out[page]["employment"]["name"],
-            "employer": out[page]["employer"]["name"],
-            "professional_roles": out[page]["professional_roles"][0]["name"],
-        }
+        
+        vacancy = {}
+
+        if not out[page]["id"]:
+            vacancy["id"] = 'null'
+        else:
+            vacancy["id"] = out[page]["id"]
+        
+        if not out[page]["name"]:
+            vacancy["name"] = 'null'
+        else:
+            vacancy["name"] = out[page]["name"]
+        
+        if not out[page]["snippet"]["responsibility"]:
+            vacancy["responsibility"] = 'null'
+        else:
+            vacancy["responsibility"] = out[page]["snippet"]["responsibility"]
+
+        if not out[page]["salary"]:
+            vacancy["salary"] = 'null'
+        else:
+            vacancy["salary"] = out[page]["salary"]["from"]
+
+        if not out[page]["address"]:
+            vacancy["address"] = 'null'
+        else:
+            vacancy["address"] = out[page]["address"]["name"]
+
+        if not out[page]["employment"]:
+            vacancy["employment"] = 'null'
+        else:
+            vacancy["employment"] = out[page]["employment"]["name"]
+        
+        if not out[page]["employer"]:
+            vacancy["employer"] = 'null'
+        else:
+            vacancy["employer"] = out[page]["employer"]["name"]
+
+        if not out[page]["professional_roles"]:
+            vacancy["professional_roles"] = 'null'
+        else:
+            vacancy["professional_roles"] = out[page]["professional_roles"][0]["name"]
+
+
+        # vacancy = {
+        #     "id": out[page]["id"],
+        #     "name": out[page]["name"],
+        #     "responsibility": out[page]["snippet"]["responsibility"],
+        #     "salary": out[page]["salary"]["from"],
+        #     "address": out[page].get("address"),
+        #     "employment": out[page]["employment"]["name"],
+        #     "employer": out[page]["employer"]["name"],
+        #     "professional_roles": out[page]["professional_roles"][0]["name"],
+        # }
     except Exception as e:
         print(e)
-    # print(out)
     return vacancy
 
-print(get_vacancy("Стажер"))
-# get_vacancy("Сварщик")
+print(json.dumps(get_vacancy("стажер C++"), indent=4, ensure_ascii=False))
+# get_vacancy("стажер C++")
