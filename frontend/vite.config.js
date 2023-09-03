11e7ec333e50886f5912541aaafdde6ca6199464
Kirill Levin <levinkirill@yandex.ru>
2023-09-03 15:21:54 +0300

    Added Docker & nginx config for frontend app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
