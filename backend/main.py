eba9fd8952e54b47ddb71dba0fceac93349c4739
Kirill Levin <levinkirill@yandex.ru>
2023-09-13 15:40:48 +0300

    CORS policy added

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine
 from models import Base
 from parsing import api_routes
 from api import routes
 
-
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "https://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(routes.router, prefix="/api", tags=["API"])
 app.include_router(api_routes.router, prefix="/parse", tags=["Parsing"])
