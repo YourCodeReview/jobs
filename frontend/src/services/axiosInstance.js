147c1415c69ebbfe4914c1e4eea3476cca3f1926
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-13 19:11:13 +0300

    edit url

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/jobs/'
+  baseURL: 'http://68.183.220.246:8000/api/jobs/'
 })
 
 export default axiosInstance
