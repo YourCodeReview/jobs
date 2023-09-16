6dda73bfc161e8e5d18ee550ac4d13f0063faa29
Khachkara <khachkara@gmail.com>
2023-09-16 11:27:43 +0300

    убрал main_words, languages_stacks

--- backend/parsing/api_routes.py
+++ backend/parsing/api_routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from crud import create_vacancy
-from .hh import get_vacancies, main_words, languages_stacks
+from .hh import get_vacancies
 
 
 
