13d9271e60e1e15a0042d64ab70c7c63617c6454
Khachkara <khachkara@gmail.com>
2023-09-17 09:55:17 +0300

    ...

--- backend/parsing/api_routes.py
+++ backend/parsing/api_routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from backend.database import get_db
 from backend.crud import create_vacancy
-from .hh import get_vacancies, main_words, languages_stacks
+from backend.parsing.hh import get_vacancies, main_words, languages_stacks
 
 
 router = APIRouter()
