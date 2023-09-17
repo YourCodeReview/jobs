03839feb2630b340df9156b1d0bced280de71548
Kirill Levin <levinkirill@yandex.ru>
2023-09-17 15:48:48 +0300

    Changed default DB to PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+# import os
+#
+# CUR_DIR = os.path.dirname(__file__)
+# DB_PATH = os.path.join(CUR_DIR, 'jobs.db')
 
-CUR_DIR = os.path.dirname(__file__)
-DB_PATH = os.path.join(CUR_DIR, 'jobs.db')
-
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+SQLALCHEMY_DATABASE_URL = "postgresql://team:password@localhost/jobs"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,  # connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
