b0f336d7a089680d54b3c8fec1b09abe30816594
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-24 14:26:05 +0300

    menu fix

--- frontend/src/components/_ui/uiMenu.vue
+++ frontend/src/components/_ui/uiMenu.vue
@@ -1,20 +1,14 @@
 <script setup>
-import { ref, computed } from 'vue'
-import { useFirebase } from '@/hooks/useFirebase'
+import { ref } from 'vue'
 
-import UiLoginButton from '@/components/_ui/uiLoginButton.vue'
-import UiLogoutButton from '@/components/_ui/uiLogoutButton.vue'
 import UiCareerButton from '@/components/_ui/uiCareerButton.vue'
 import uiAddVacancyButton from '@/components/_ui/uiAddVacancyButton.vue'
 
 defineProps({
   list: Array
 })
-
-const auth = useFirebase()
 const menu = ref(false)
 
-const showLoginButton = computed(() => !auth.isLoggedIn.value)
 </script>
 
 <template>
@@ -31,9 +25,7 @@ const showLoginButton = computed(() => !auth.isLoggedIn.value)
         </v-btn>
       </template>
       <v-card class="d-flex flex-column align-center pa-4 bg-white" rounded="lg">
-        <UiCareerButton variant="flat" block />
-        <UiLoginButton v-if="showLoginButton" variant="flat" block />
-        <UiLogoutButton v-else variant="flat" block />
+        <UiCareerButton v-if="$vuetify.display.xs" variant="flat" block />
         <slot />
         <uiAddVacancyButton variant="flat" block />
       </v-card>
