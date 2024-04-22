b4f93c3297593c549bd505a7c33568ef0a177582
Рябов Павел <ryabovpavel@Rabovs-MacBook-Air.local>
2024-04-22 21:01:43 +0300

    Новые фильтры

--- backend/crud.py
+++ backend/crud.py
@@ -1,19 +1,46 @@
-from sqlalchemy.orm import Session
 from sqlalchemy import func
+from sqlalchemy.orm import Session
 
 from schemas import VacancyCreate
 from models import Vacancy
 
 
-def get_vacancies(db: Session, skip: int = 0, limit: int = 10):
+def get_vacancies(db: Session, skip, limit: int):
     """ Retrieve all vacancies with pagination """
     vacancies = db.query(Vacancy).offset(skip).limit(limit).all()
     total_count = db.query(func.count(Vacancy.id)).scalar()
     return total_count, vacancies
 
 
-def get_vacancies_with_specialities(db: Session, skip: int, limit: int, specialities: list):
-    """ Retrieve vacancies filtered by speciality along with the total count """
+def get_vacancies_by_filters(
+        db: Session,
+        skip: int,
+        limit: int,
+        specialities: list[str],
+        internship: bool,
+        remote: bool,
+        location: list[str]
+
+):
+    """ Retrieve filtred vacancies """
+    query = db.query(Vacancy)
+    if specialities:
+        query = query.filter(Vacancy.speciality.in_(specialities))
+    if internship:
+        query = query.filter(Vacancy.internship)
+    if remote:
+        query = query.filter(Vacancy.remote)
+    if location:
+        query = query.filter(Vacancy.location.in_(location))
+    vacancies = query.offset(skip).limit(limit).all()
+    total_count = query.count()
+    return total_count, vacancies
+
+
+def get_vacancies_with_specialities(
+        db: Session, skip: int, limit: int, specialities: list
+):
+    """Retrieve vacancies filtered by speciality along with the total count """
     query = db.query(Vacancy).filter(Vacancy.speciality.in_(specialities))
     total_count = query.count()  # Count the total matching rows
     vacancies = query.offset(skip).limit(limit).all()
