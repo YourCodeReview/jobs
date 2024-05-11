0adab8f71ed4036af13706209c935c6c4a8de8f0
Valery Tsishchanka <houlett95@gmail.com>
2024-05-11 21:10:35 +0300

    1) moved the variable start inside the function parse_all_page, 2) removed print(detail_url) from the function parse_vacancy, 3) made all quotes in the file single quotes, 4) removed the call to the function parse_all_page

--- backend/parsing/link_vac_bs4.py
+++ backend/parsing/link_vac_bs4.py
@@ -1,10 +1,10 @@
-from bs4 import BeautifulSoup
-import requests
-import re
 import json
+import re
 import time
 
-start = -25
+import requests
+from bs4 import BeautifulSoup
+
 
 def vacancy_validation(vacancy_title):
     languages_stacks = [
@@ -44,15 +44,19 @@ def vacancy_validation(vacancy_title):
     else:
         return False
 
+
 def parse_vacancy(vacancy):
-    title = vacancy.find('h3', class_='base-search-card__title').text
+    title = vacancy.find("h3", class_="base-search-card__title").text
     if not vacancy_validation(title):
         return None
 
-    company_name = vacancy.find('h4', class_='base-search-card__subtitle').text
-    title = vacancy.find('h3', class_='base-search-card__title').text
-    location = vacancy.find('span', class_='job-search-card__location').text
-    url_vacancy = vacancy.find("a", class_="base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]").get("href")
+    company_name = vacancy.find("h4", class_="base-search-card__subtitle").text
+    title = vacancy.find("h3", class_="base-search-card__title").text
+    location = vacancy.find("span", class_="job-search-card__location").text
+    url_vacancy = vacancy.find(
+        "a",
+        class_="base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]",
+    ).get("href")
 
     pattern_before_position = r".{10}(?=\?position)"
     pattern_trackingid_position = r"trackingId=([^&]+)&trk"
@@ -63,16 +67,17 @@ def parse_vacancy(vacancy):
     ten_chars_before_refid = match_before_refid.group(0)
     tracking_id = match_trackingid_position.group(1)
     detail_url = f"https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{ten_chars_before_refid}?trackingId={tracking_id}"
-    print(detail_url)
 
     r_description = requests.get(detail_url)
     soup_detail = BeautifulSoup(r_description.text, "lxml")
-    div_element = soup_detail.select_one(".show-more-less-html__markup.show-more-less-html__markup--clamp-after-5")
+    div_element = soup_detail.select_one(
+        ".show-more-less-html__markup.show-more-less-html__markup--clamp-after-5"
+    )
 
     r_text = []
-    for tag in div_element.find_all(['p', 'li']):
-        if tag.name == 'li':
-            r_text.append('• ' + tag.get_text())
+    for tag in div_element.find_all(["p", "li"]):
+        if tag.name == "li":
+            r_text.append("• " + tag.get_text())
         else:
             r_text.append(tag.get_text())
 
@@ -92,32 +97,44 @@ def parse_one_page(all_vacancy_on_page):
     for vacancy in all_vacancy_on_page:
         parse_vacancy(vacancy)
 
-def parse_all_page(start):
+
+def parse_all_page():
+    start = 0
     while start < 1000:
-        url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=&location=&geoId=101728296&trk=guest_homepage-basic_jobs-search-bar_search-submit&start=" + str(start)
+        url = (
+            "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=&location=&geoId=101728296&trk=guest_homepage-basic_jobs-search-bar_search-submit&start="
+            + str(start)
+        )
         print(f"Обрабатывается страница {url}")
         r = requests.get(url)
         print(f"Status Code: {r.status_code}")
         print(f"Status Message: {r.reason}")
         soup = BeautifulSoup(r.text, "lxml")
-        all_vacancy_on_page = soup.findAll("div", class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card")
+        all_vacancy_on_page = soup.findAll(
+            "div",
+            class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card",
+        )
         parse_one_page(all_vacancy_on_page)
         start += 25
         time.sleep(1)
     start = 0
     print("Начинаю поиск по Беларуси")
     while start < 1000:
-        url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=&location=&geoId=101705918&trk=public_jobs_jobs-search-bar_search-submit&start=" + str(start)
+        url = (
+            "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=&location=&geoId=101705918&trk=public_jobs_jobs-search-bar_search-submit&start="
+            + str(start)
+        )
         print(f"Обрабатывается страница {url}")
         r = requests.get(url)
         print(f"Status Code: {r.status_code}")
         print(f"Status Message: {r.reason}")
         soup = BeautifulSoup(r.text, "lxml")
-        all_vacancy_on_page = soup.findAll("div", class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card")
+        all_vacancy_on_page = soup.findAll(
+            "div",
+            class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card",
+        )
         parse_one_page(all_vacancy_on_page)
         start += 25
         time.sleep(1)
 
 
-
-parse_all_page(0)
