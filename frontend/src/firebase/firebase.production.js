0092896e6ab1d45ee4d2eb4381f66d0891b58fdc
Anton Kulagin <anton-kulagin-v@mail.ru>
2024-08-11 21:49:19 +0300

    #45 ADD: Добавление номера телефона и email пользователя в базу данных firebase

---
+++ frontend/src/firebase/firebase.production.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtDB_K432v81AcCv3x-jvfs-wymseFh8U",
+  authDomain: "codereview-jobs.firebaseapp.com",
+  projectId: "codereview-jobs",
+  storageBucket: "codereview-jobs.appspot.com",
+  messagingSenderId: "1056554077979",
+  appId: "1:1056554077979:web:dd86c5c5976c9fdc44cdbe",
+  measurementId: "G-10PZV4WFCG"
+}
+
+initializeApp(firebaseConfig)
