7cdecfbaa9ad5556c66e4c30ad0e55ccc978eafd
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-08-18 06:42:07 +0800

    #35 STL: Обновлен способ импорта данных

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 # import os
 #
 # CUR_DIR = os.path.dirname(__file__)
