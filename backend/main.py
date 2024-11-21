88b772dcb3dee2654874fcab8f8cac9802075352
millevi <21953313+millevi@users.noreply.github.com>
2024-11-22 01:48:34 +0300

    [CHN]: Вернул CORS

--- backend/main.py
+++ backend/main.py
@@ -29,12 +29,12 @@ origins = [
     "http://127.0.0.1:8080",
 ]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routes.router, prefix="/api", tags=["API"])
