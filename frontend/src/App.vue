da1d2a634012b06b57c232ef135e32f57b360ce4
gra4evyakov <gra4evyakov@yandex.ru>
2024-12-03 12:47:59 +0300

    fix: remove advertising dialog

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,30 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import uiAdvertisingDialog from '@/components/_ui/uiAdvertisingDialog.vue'
+// import uiAdvertisingDialog from '@/components/_ui/uiAdvertisingDialog.vue'
 
-const dialog = ref(false)
+// const dialog = ref(false)
 
-const setDialogIsOpen = () => {
-  dialog.value = true
-  localStorage.setItem('advertising-shown', 'true')
-}
+// const setDialogIsOpen = () => {
+//   dialog.value = true
+//   localStorage.setItem('advertising-shown', 'true')
+// }
 
 onMounted(() => {
-  const advertisingShown = localStorage.getItem('advertising-shown')
-  if (advertisingShown) return
-
-  document.addEventListener('mousemove', (event) => {
-    const advertisingShown = localStorage.getItem('advertising-shown')
-    if (advertisingShown) return
-    console.log(event.clientY)
-
-    if (event.clientY <= 12) {
-      setDialogIsOpen()
-    }
-  })
-
-  setTimeout(() => {
-    const advertisingShown = localStorage.getItem('advertising-shown')
-    if (advertisingShown) return
-
-    setDialogIsOpen()
-  }, 45000)
+  // const advertisingShown = localStorage.getItem('advertising-shown')
+  // if (advertisingShown) return
+  // document.addEventListener('mousemove', (event) => {
+  //   const advertisingShown = localStorage.getItem('advertising-shown')
+  //   if (advertisingShown) return
+  //   console.log(event.clientY)
+  //   if (event.clientY <= 12) {
+  //     setDialogIsOpen()
+  //   }
+  // })
+  // setTimeout(() => {
+  //   const advertisingShown = localStorage.getItem('advertising-shown')
+  //   if (advertisingShown) return
+  //   setDialogIsOpen()
+  // }, 45000)
 })
 </script>
 
@@ -36,6 +32,6 @@ onMounted(() => {
   <v-app id="inspire" class="bg-grey-lighten-4 position-relative">
     <router-view />
 
-    <uiAdvertisingDialog v-model="dialog" />
+    <!-- <uiAdvertisingDialog v-model="dialog" /> -->
   </v-app>
 </template>
